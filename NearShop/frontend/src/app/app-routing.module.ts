fb52915d5018f8cde697461a31e6f52aa8e1bc4d
fradjmili <fradj.mili@esprit.tn>
2022-04-30 22:17:24 +0200

    homePage_And_AddProductRoute

--- NearShop/frontend/src/app/app-routing.module.ts
+++ NearShop/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomePageComponent},
+  {path:'AddProduct',component:AddProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- NearShop/frontend/src/app/app.module.ts
+++ NearShop/frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddProductComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
