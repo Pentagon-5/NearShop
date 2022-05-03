14444b18758cc93065935668d8e0b9fdb116e669
unknown <brkhayreddine@gmail.com>
2022-05-03 23:48:19 +0100

    search

--- NearShop/frontend/src/app/search/search.component.ts
+++ NearShop/frontend/src/app/search/search.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  data:any= []
+      amount:any= 0
+         search: any =""
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  filtredProduct (){
+    return this.data.filter((element:any)=>{
+      return element.title.match(this.search)
+    })
   }
 
 }
