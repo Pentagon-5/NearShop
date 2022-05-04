c4fb000194f965e3d6e8a5eb5c0aa3cabcd59797
haythem boudhina <zayanihaythem1@gmail.com>
2022-05-04 22:41:01 +0100

    about us

--- NearShop/frontend/src/app/app-routing.module.ts
+++ NearShop/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
+import { AboutUsComponent } from "./about-us/about-us.component";
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {path:'',component:HomePageComponent},
-  {path:'AddProduct',component:AddProductComponent}
+  {path:'AddProduct',component:AddProductComponent},
+  {path:'AboutUs',component:AboutUsComponent}
+
 ];
 
 @NgModule({
